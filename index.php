3bac36026fb35a3b9241d08c28a7b5d6b6a9f63c
HenriqueCeZ <henriquecezar.jp@gmail.com>
2021-09-28 23:36:39 -0300

    teceiro commit

--- index.php
+++ index.php
@@ -8,13 +8,37 @@
 
 
 	try {
-		$pdo = new PDO($dsn,$dbuser,$dbpass);
-		$sql = "DELETE FROM usuarios WHERE id = '7' ";
-		$sql = $pdo->query($sql);
+	$pdo = new PDO($dsn,$dbuser,$dbpass);
 
+	$nome = 'Cezar';
+	$id = 5;
 
+	$sql = 	"UPDATE usuarios SET nome = :nome WHERE id = :id";
+	$sql =  $pdo->prepare($sql);
+	$sql->bindValue(':nome',$nome);
+	$sql->bindValue(':id',$id);
+	$sql->execute();	
+	$sql = "SELECT nome FROM usuarios WHERE id = :id";
+	$sql =  $pdo->prepare($sql);
+	$sql->bindValue(':id',$id);
+	$sql->execute();
+
+		if($sql->rowCount() > 0 ){
+
+			foreach($sql->fetchAll() as $usuarios) {
+				echo("nome : ".$usuarios["nome"]);
+				# code...
+			}
+
+	
 		
+	}else{
+
+
+		echo 'Erro';
 	}
+
+}
 catch(PDOexcepetion $e){
 
 		echo "falhou: ".$e->getMessege();
@@ -25,3 +49,4 @@ catch(PDOexcepetion $e){
 
 
 ?>
+
